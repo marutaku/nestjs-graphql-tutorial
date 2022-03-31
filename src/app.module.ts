821720109a1110643ad0d7e10fb85d8dc9ea32d3
Takumi Maruyama <satouredstone@gmail.com>
2022-03-31 14:17:03 +0000

    設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // この方法では，起動の度にスキーマを作成している
+      // 手動でスキーマを作る方法は以下を参照する
+      // https://docs.nestjs.com/graphql/quick-start#schema-first
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
